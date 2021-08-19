9c809ba372b53912faf147bff250a1b4d827b7c6
Habid B Espinosa <hab.bad@hotmail.com>
2021-08-19 09:49:18 +0200

    contextAdedd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import './App.css';
+import './sass/App.scss';
+import { useContext } from 'react'
+import { UserContext, UserContextProvider } from './context/UserContext';
 
 function App() {
+  const { records } = useContext(UserContext)
+
   return (
     <div className="App">
       <h1>React record store</h1>
---
+++ src/context/UserContext.js
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react";
+
+export const UserContext = createContext();
+export const UserContextProvider = (props) => {
+    const [records, setRecords] = useState([]);
+
+    return (
+        <UserContext.Provider
+            value={
+                {
+                    records, setRecords
+                }
+            }
+        >
+            {props.children}
+        </UserContext.Provider>
+    )
+}
